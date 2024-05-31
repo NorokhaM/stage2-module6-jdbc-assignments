363c3bcf5d9c051e0c3af74b6fd7424eaed929a9
Maksym Norokha <maxnorohich@gmail.com>
2024-05-31 13:52:40 +0200

    123

--- src/main/java/jdbc/CustomConnector.java
+++ src/main/java/jdbc/CustomConnector.java
@@ -1,14 +1,15 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class CustomConnector {
     public Connection getConnection(String url) throws SQLException{
-        return getConnection(url);
+        return DriverManager.getConnection(url);
     }
 
-    public Connection getConnection(String url, String user, String password)  throws SQLException   {
-        return getConnection(url, user, password);
+    public Connection getConnection(String url, String user, String password) throws SQLException  {
+        return DriverManager.getConnection(url, user, password);
     }
 }
